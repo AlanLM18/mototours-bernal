3c59256a4f160cb5f4276d58326a0c931c6c840d
AlanLM18 <alanlm.ti23@utsjr.edu.mx>
2024-12-19 15:37:01 -0600

    catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,14 +4,14 @@ const router = express.Router();
 // Datos de ejemplo para el catálogo
 const rzrVehicles = [
   { id: 5, name: "polaris crew pro xd", price: 1200, image: "/images/polaris.jpg", description: "RZR off road de 4 plazas" },
-  { id: 6, name: "rzr polaris 1000 xr ", price: 1200, image: "/images/rzr1000.jpg", description: "RZR off road de 4 plazas y buena potencia" }
+  { id: 6, name: "rzr polaris 1000 xr ", price: 1200, image: "/images/rzr1000.jpg", description: "RZR off road de 4 plazas" }
 ];
 
 const cuatrimotosVehicles = [
-  { id: 2, name: "Italika atv200", price: 500, image: "/images/atv200.jpg", description: "Cuatrimoto italica de 200cc, grande" },
-  { id: 1, name: "Italika atv180", price: 500, image: "/images/atv180.jpg", description: "Cuatrimoto italica de 180cc, mediana" },
-  { id: 3, name: "Italika atv180 c", price: 500, image: "/images/atv180c.jpg", description: "Cuatrimoto italica de 180cc, mediana" },
-  { id: 4, name: "Italika atv150", price: 500, image: "/images/atv150.jpg", description: "Cuatrimoto italica de 150cc, chica" }
+  { id: 2, name: "Italika atv200", price: 500, image: "/images/atv200.jpg", description: "Cuatrimoto italica de 200cc" },
+  { id: 1, name: "Italika atv180", price: 500, image: "/images/atv180.jpg", description: "Cuatrimoto italica de 180cc" },
+  { id: 3, name: "Italika atv180 c", price: 500, image: "/images/atv180c.jpg", description: "Cuatrimoto italica de 180cc" },
+  { id: 4, name: "Italika atv150", price: 500, image: "/images/atv150.jpg", description: "Cuatrimoto italica de 150cc" }
 ];
 
 // Ruta para mostrar el catálogo
